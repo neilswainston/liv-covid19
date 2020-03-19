85cb13d6f2f1521c09b7b513fa8cf87821beb478
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-03-19 22:44:51 +0000

    Added reagent dispension.

--- liv_covid19/opentrons/cdna_first.py
+++ liv_covid19/opentrons/cdna_first.py
@@ -17,21 +17,9 @@ metadata = {'apiLevel': '2.0',
             'author': 'Neil Swainston <neil.swainston@liverpool.ac.uk>',
             'description': 'simple'}
 
-_TIP_RACKS = {
-    'type': 'opentrons_96_filtertiprack_20ul',
-    'start_at_tip': 'A1'
-}
-
-_PIPETTES = {
-    'right': 'p50_multi'
-}
-
 _REAGENT_PLATE = {
     'type': 'nest_12_reservoir_15ml',
-    'components': {'ssiv_buffer': 'A1',
-                   'dtt': 'B1',
-                   'ribonuclease_inhibitor': 'C1',
-                   'reverse_transcriptase': 'D1'}
+    'components': {'primer_mix': 'A1'}
 }
 
 _SRC_PLATES = {
@@ -46,125 +34,81 @@ _DST_PLATES = {
 
 def run(protocol):
     '''Run protocol.'''
-    writer = ProtocolWriter(protocol)
-    writer.write()
+    assert _SRC_PLATES['count'] < 4
+
+    # Setup tip racks:
+    reag_tip_rack = protocol.load_labware(
+        'opentrons_96_filtertiprack_20ul', 10)
+
+    # Add source and destination tip-racks to deck positions 1 -> n:
+    src_tip_racks = [protocol.load_labware(
+        'opentrons_96_filtertiprack_20ul', idx + 1)
+        for idx in range(_SRC_PLATES['count'])]
+
+    # Add pipette:
+    pipette = protocol.load_instrument(
+        'p50_multi', 'left', tip_racks=list([reag_tip_rack] + src_tip_racks))
+
+    # Setup plates:
+    _add_plates(protocol)
+
+    # Add operations:
+    add_operations(protocol, pipette, src_tip_racks)
 
 
-class ProtocolWriter():
-    '''Class to write protocol.'''
+def add_operations(protocol, pipette, src_tip_racks):
+    '''Add operations.'''
+    for plt_idx in range(_SRC_PLATES['count']):
+        # Transfer reagents:
+        dst_plt = _get_obj(protocol, 'smpl_dst_%i' % (plt_idx + 1))
+        reag_plt, reag_well = _get_plate_well(protocol, 'primer_mix')
 
-    def __init__(self, protocol):
-        self.__protocol = protocol
+        pipette.distribute(8.0,
+                           reag_plt[reag_well],
+                           dst_plt.wells())
 
-    def write(self):
-        '''Write protocol.'''
+        # Transfer RNA samples:
+        pipette.starting_tip = src_tip_racks[0]['A1']
 
-        # Setup:
-        self.__do_setup()
+        src_plt = _get_obj(protocol, 'smpl_src_%i' % (plt_idx + 1))
 
-        # Add functions:
-        self.__add_funcs()
-
-    def __do_setup(self):
-        '''Setup.'''
-
-        # Setup tip racks:
-        tip_racks = self.__add_tip_racks()
-
-        # Setup pipettes:
-        self.__add_pipettes(tip_racks)
-
-        # Setup plates:
-        self.__add_plates()
-
-    def __add_tip_racks(self):
-        '''Add tip racks.'''
-        tip_racks = {}
-
-        for _ in range(_SRC_PLATES['count']):
-            tip_rack = self.__protocol.load_labware(
-                _TIP_RACKS['type'],
-                self.__next_empty_slot())
-
-            tip_racks[tip_rack] = _TIP_RACKS.get('start_at_tip', 'A1')
-
-        return tip_racks
-
-    def __add_pipettes(self, tip_racks):
-        '''Add pipettes.'''
-        for mount, instrument_name in _PIPETTES.items():
-            pipette = self.__protocol.load_instrument(
-                instrument_name, mount, tip_racks=list(tip_racks))
-
-            for tip_rack, start_at_tip in tip_racks.items():
-                pipette.starting_tip = tip_rack[start_at_tip]
-                break
-
-    def __add_plates(self):
-        '''Add plates.'''
-        # Add reagent plate:
-        self.__protocol.load_labware(_REAGENT_PLATE['type'],
-                                     self.__next_empty_slot(),
-                                     'reagents')
-
-        # Add source and destination plates:
-        for plt_idx in range(_SRC_PLATES['count']):
-            self.__protocol.load_labware(_SRC_PLATES['type'],
-                                         self.__next_empty_slot(),
-                                         'smpl_src_%i' % (plt_idx + 1))
-
-            self.__protocol.load_labware(_SRC_PLATES['type'],
-                                         self.__next_empty_slot(),
-                                         'smpl_dst_%i' % (plt_idx + 1))
-
-    def __add_funcs(self):
-        '''Add functions.'''
-        pipette = get_pipette(self.__protocol, channels=8)
-
-        for plt_idx in range(_SRC_PLATES['count']):
-            src_plt = get_obj('smpl_src_%i' % (plt_idx + 1),
-                              self.__protocol)
-
-            dst_plt = get_obj('smpl_dst_%i' % (plt_idx + 1),
-                              self.__protocol)
-
-            for src_col, dst_col in zip(src_plt.columns(), dst_plt.columns()):
-                pipette.transfer(5.0, src_col, dst_col)
-
-    def __next_empty_slot(self):
-        '''Get next empty slot.'''
-        for slot, obj in self.__protocol.deck.items():
-            if not obj:
-                return slot
-
-        raise IndexError('Insufficient slots on desk')
-
-    def __get_plate_well(self, component):
-        '''Get plate well.'''
-        if component in _REAGENT_PLATE.get('components', []):
-            return get_obj(_REAGENT_PLATE['name'], self.__protocol), \
-                _REAGENT_PLATE['components']
-
-        return None
+        for src_col, dst_col in zip(src_plt.columns(), dst_plt.columns()):
+            pipette.transfer(5.0, src_col, dst_col)
 
 
-def get_obj(obj_name, protocol):
+def _add_plates(protocol):
+    '''Add plates.'''
+    # Add reagent plate:
+    protocol.load_labware(_REAGENT_PLATE['type'], 11, 'reagent_plt')
+
+    # Add source and destination plates:
+    for plt_idx in range(_SRC_PLATES['count']):
+        protocol.load_labware(_SRC_PLATES['type'],
+                              plt_idx + 4,
+                              'smpl_src_%i' % (plt_idx + 1))
+
+        protocol.load_labware(_DST_PLATES['type'],
+                              plt_idx + 7,
+                              'smpl_dst_%i' % (plt_idx + 1))
+
+
+def _get_plate_well(protocol, reagent):
+    '''Get reagent plate-well.'''
+    well = _REAGENT_PLATE['components'].get(reagent, None)
+
+    if well:
+        return _get_obj(protocol, 'reagent_plt'), well
+
+    return None
+
+
+def _get_obj(protocol, obj_name):
     '''Get object.'''
     for val in protocol.deck.values():
         if val and val.name == obj_name:
             return val
 
     return None
-
-
-def get_pipette(protocol, channels):
-    '''Get appropriate pipette for volume.'''
-
-    # Ensure pipette is appropriate for minimum volume:
-    valid_pipettes = [pip for pip in protocol.loaded_instruments.values()
-                      if pip.channels == channels]
-
-    return valid_pipettes[0] if valid_pipettes else None
 
 
 def main():
