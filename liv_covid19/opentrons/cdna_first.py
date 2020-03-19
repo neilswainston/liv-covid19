7f8155cbd7e3ca17ccceca298c10b87712a182db
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-03-19 23:08:08 +0000

    Updated deck format.

--- liv_covid19/opentrons/cdna_first.py
+++ liv_covid19/opentrons/cdna_first.py
@@ -17,13 +17,15 @@ metadata = {'apiLevel': '2.0',
             'author': 'Neil Swainston <neil.swainston@liverpool.ac.uk>',
             'description': 'simple'}
 
+_TIP_RACK_TYPE = 'opentrons_96_filtertiprack_20ul'
+
 _REAGENT_PLATE = {
     'type': 'nest_12_reservoir_15ml',
     'components': {'primer_mix': 'A1'}
 }
 
 _SRC_PLATES = {
-    'count': 1,
+    'count': 3,
     'type': '4ti_96_wellplate_350ul'
 }
 
@@ -37,49 +39,34 @@ def run(protocol):
     assert _SRC_PLATES['count'] < 4
 
     # Setup tip racks:
-    reag_tip_rack = protocol.load_labware(
-        'opentrons_96_filtertiprack_20ul', 10)
-
-    # Add source and destination tip-racks to deck positions 1 -> n:
-    src_tip_racks = [protocol.load_labware(
-        'opentrons_96_filtertiprack_20ul', idx + 1)
-        for idx in range(_SRC_PLATES['count'])]
+    reag_tip_rack, src_tip_racks = _add_tip_racks(protocol)
 
     # Add pipette:
     pipette = protocol.load_instrument(
         'p50_multi', 'left', tip_racks=list([reag_tip_rack] + src_tip_racks))
 
     # Setup plates:
-    _add_plates(protocol)
+    reag_plt = _add_plates(protocol)
 
     # Add operations:
-    add_operations(protocol, pipette, src_tip_racks)
+    _add_operations(protocol, pipette, src_tip_racks, reag_plt)
 
 
-def add_operations(protocol, pipette, src_tip_racks):
-    '''Add operations.'''
-    for plt_idx in range(_SRC_PLATES['count']):
-        # Transfer reagents:
-        dst_plt = _get_obj(protocol, 'smpl_dst_%i' % (plt_idx + 1))
-        reag_plt, reag_well = _get_plate_well(protocol, 'primer_mix')
+def _add_tip_racks(protocol):
+    '''Add tip racks.'''
+    reag_tip_rack = protocol.load_labware(_TIP_RACK_TYPE, 10)
 
-        pipette.distribute(8.0,
-                           reag_plt[reag_well],
-                           dst_plt.wells())
+    # Add source and destination tip-racks to deck positions 1 -> n:
+    src_tip_racks = [protocol.load_labware(_TIP_RACK_TYPE, idx + 1)
+                     for idx in range(_SRC_PLATES['count'])]
 
-        # Transfer RNA samples:
-        pipette.starting_tip = src_tip_racks[0]['A1']
-
-        src_plt = _get_obj(protocol, 'smpl_src_%i' % (plt_idx + 1))
-
-        for src_col, dst_col in zip(src_plt.columns(), dst_plt.columns()):
-            pipette.transfer(5.0, src_col, dst_col)
+    return reag_tip_rack, src_tip_racks
 
 
 def _add_plates(protocol):
     '''Add plates.'''
     # Add reagent plate:
-    protocol.load_labware(_REAGENT_PLATE['type'], 11, 'reagent_plt')
+    reag_plt = protocol.load_labware(_REAGENT_PLATE['type'], 11)
 
     # Add source and destination plates:
     for plt_idx in range(_SRC_PLATES['count']):
@@ -91,13 +78,35 @@ def _add_plates(protocol):
                               plt_idx + 7,
                               'smpl_dst_%i' % (plt_idx + 1))
 
+    return reag_plt
 
-def _get_plate_well(protocol, reagent):
+
+def _add_operations(protocol, pipette, src_tip_racks, reag_plt):
+    '''Add operations.'''
+
+    # Transfer reagents:
+    for plt_idx in range(_SRC_PLATES['count']):
+        dst_plt = _get_obj(protocol, 'smpl_dst_%i' % (plt_idx + 1))
+        _, reag_well = _get_plate_well(reag_plt, 'primer_mix')
+
+        pipette.distribute(8.0, reag_plt[reag_well], dst_plt.wells())
+
+    # Transfer RNA samples:
+    pipette.starting_tip = src_tip_racks[0]['A1']
+
+    for plt_idx in range(_SRC_PLATES['count']):
+        src_plt = _get_obj(protocol, 'smpl_src_%i' % (plt_idx + 1))
+
+        for src_col, dst_col in zip(src_plt.columns(), dst_plt.columns()):
+            pipette.transfer(5.0, src_col, dst_col)
+
+
+def _get_plate_well(reag_plt, reagent):
     '''Get reagent plate-well.'''
     well = _REAGENT_PLATE['components'].get(reagent, None)
 
     if well:
-        return _get_obj(protocol, 'reagent_plt'), well
+        return reag_plt, well
 
     return None
 
