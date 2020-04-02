e9846e47a55c17793e40438c3278d6ddea3bd439
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-04-02 15:00:03 +0100

    Added consideration of Mosquito max pipetting volume.

--- liv_covid19/normal.py
+++ liv_covid19/normal.py
@@ -23,7 +23,7 @@ def run(in_filename, out_dir):
     in_df = 50 / in_df
 
     # Check validity:
-    assert in_df.max().max() < 12.5
+    assert in_df.max().max() <= 12.5
 
     mantis_df = 12.5 - in_df
 
@@ -52,29 +52,46 @@ def _get_data(in_filename):
     return in_df
 
 
-def _get_mosquito(in_df):
+def _get_mosquito(in_df, max_vol=12000):
     '''Get Mosquito worklist.'''
+
+    # Convert to tabular data:
     n, k = in_df.shape
 
     data = {'Nanolitres': in_df.to_numpy().ravel('F') * 1000,
             'Column': np.asarray(in_df.columns).repeat(n),
             'Row': np.tile(np.asarray(in_df.index), k)}
 
-    mosquito_df = pd.DataFrame(data, columns=['Column', 'Row', 'Nanolitres'])
-    mosquito_df['Position'] = 2
-    mosquito_df['Column dest'] = mosquito_df['Column']
-    mosquito_df['Row dest'] = mosquito_df['Row']
-    mosquito_df['Position dest'] = 3
+    df = pd.DataFrame(data, columns=['Column', 'Row', 'Nanolitres'])
 
-    mosquito_df = mosquito_df[['Position', 'Column', 'Row',
-                               'Position dest', 'Column dest', 'Row dest',
-                               'Nanolitres']]
+    # Add missing plate positions and destinations:
+    df['Position'] = 2
+    df['Column dest'] = df['Column']
+    df['Row dest'] = df['Row']
+    df['Position dest'] = 3
 
-    mosquito_df.columns = ['Position', 'Column', 'Row',
-                           'Position', 'Column', 'Row',
-                           'Nanolitres']
+    # Check against maximum volume:
+    over_max_df = df[df['Nanolitres'] > max_vol].copy()
 
-    return mosquito_df
+    while not over_max_df.empty:
+        df.loc[df['Nanolitres'] > max_vol, 'Nanolitres'] = \
+            df.loc[df['Nanolitres'] > max_vol, 'Nanolitres'] / 2
+
+        over_max_df.loc[:, 'Nanolitres'] = over_max_df.loc[:, 'Nanolitres'] / 2
+        df = df.append(over_max_df).sort_index()
+
+        over_max_df = df[df['Nanolitres'] > max_vol]
+
+    # Reorder and rename columns:
+    df = df[['Position', 'Column', 'Row',
+             'Position dest', 'Column dest', 'Row dest',
+             'Nanolitres']]
+
+    df.columns = ['Position', 'Column', 'Row',
+                  'Position', 'Column', 'Row',
+                  'Nanolitres']
+
+    return df
 
 
 def main(args):
