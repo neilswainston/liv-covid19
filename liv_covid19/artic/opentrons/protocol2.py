7f3951e3390e3f55f135324d2a6382e03e97aac2
Neil Swainston <neil.swainston@liverpool.ac.uk>
2020-04-05 13:10:30 +0100

    Updated barcoding.

--- liv_covid19/artic/opentrons/protocol2.py
+++ liv_covid19/artic/opentrons/protocol2.py
@@ -22,8 +22,10 @@ metadata = {'apiLevel': '2.1',
 
 _REAGENT_PLATE = {
     'type': 'nest_12_reservoir_15ml',
-    'components': {'barcodes': 'A1',
-                   'ligation_mastermix': 'A2',
+    'components': {'barcodes_1': 'A1',
+                   'barcodes_2': 'A2',
+                   'barcodes_3': 'A3',
+                   'ligation_mastermix': 'A4',
                    'beads': 'A5',
                    'ethanol_1': 'A6',
                    'ethanol_2': 'A7',
@@ -57,8 +59,7 @@ def run(protocol):
     Press Continue.''')
 
     # Barcode ligation:
-    _barcode(protocol, therm_mod, p10_single, p10_multi, reag_plt, src_plt,
-             dst_plt)
+    _barcode(protocol, therm_mod, p10_multi, reag_plt, src_plt, dst_plt)
 
 
 def _setup(protocol):
@@ -127,8 +128,7 @@ def _normalise(protocol, therm_mod, p10_single, reag_plt, src_plt, dst_plt):
     therm_mod.open_lid()
 
 
-def _barcode(protocol, therm_mod, p10_single, p10_multi, reag_plt, src_plt,
-             dst_plt):
+def _barcode(protocol, therm_mod, p10_multi, reag_plt, src_plt, dst_plt):
     '''Barcode.'''
     protocol.comment('\nBarcode samples')
 
@@ -146,8 +146,11 @@ def _barcode(protocol, therm_mod, p10_single, p10_multi, reag_plt, src_plt,
     # Add barcodes:
     protocol.comment('\nAdd barcodes')
 
-    _distribute_reagent(p10_multi, reag_plt, dst_plt, [1], 'barcodes', 2.5,
-                        top=0)
+    for barcode_idx in [0, 1, 2]:
+        col_idxs = [idx + 1 for idx in range(barcode_idx, _get_num_cols(), 3)]
+
+        _distribute_barcodes(p10_multi, reag_plt, dst_plt, col_idxs,
+                             'barcodes_%i' % (barcode_idx + 1), 2.5)
 
     # Add ligation mastermix:
     protocol.comment('\nAdd ligation mastermix')
@@ -203,6 +206,24 @@ def _transfer_samples(pipette, src_plt, dst_plt, src_col, dst_col, vol):
             src_plt.columns()[src_col - 1:src_col - 1 + num_cols],
             dst_plt.columns()[dst_col - 1:dst_col - 1 + num_cols]):
         pipette.transfer(vol, src, dst, mix_after=(3, vol), disposal_volume=0)
+
+
+def _distribute_barcodes(pipette, reag_plt, dst_plt, dst_cols, reagent, vol):
+    '''Distribute barcodes.'''
+    pipette.pick_up_tip()
+
+    _, reag_well = _get_plate_well(reag_plt, reagent)
+
+    dest_cols = [dst_plt.rows_by_name()['A'][col_idx - 1]
+                 for col_idx in dst_cols]
+
+    pipette.distribute(vol,
+                       reag_plt.wells_by_name()[reag_well],
+                       [well.top() for well in dest_cols],
+                       new_tip='never',
+                       disposal_volume=0)
+
+    pipette.drop_tip()
 
 
 def _distribute_reagent(pipette, reag_plt, dst_plt, dst_cols, reagent, vol,
